dfbc45aa9d44f917cee59bb90ee73dd5d35f588f
abhinavchandoli <abhinavchandoli1@gmail.com>
2024-12-14 19:11:03 -0500

    readme

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -9,7 +9,7 @@ const { Title, Paragraph, Text } = Typography;
 const teamMembers = [
   {
     name: 'Abhinav Chandoli',
-    role: 'Data Analysis, Data Visualization, Frontend Web Development, Backend Development',
+    role: 'Data Analysis, Data Visualization, Frontend Web Development, Machine Learning',
     linkedin: 'https://www.linkedin.com/in/abhinav-chandoli-03',
     github: 'https://github.com/abhinavchandoli',
   },
