6567a9bf5db6e80dbcaffc30223a7796dceb93b8
abhinavchandoli <abhinavchandoli1@gmail.com>
2024-12-16 04:54:16 -0500

    revert styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-// Use NavLink instead of Link for nav items
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -17,10 +16,9 @@ const Header = () => {
         className="rounded-3 shadow"
       >
         <Container className="justify-content-between">
-          {/* Keep the brand as a plain Link so it never gets 'active' styling */}
           <Navbar.Brand as={Link} to="/" className="fw-bold">
             <img
-              src="/monogram.png"
+              src="/monogram.png" // Replace with your logo path
               width="30"
               height="30"
               className="d-inline-block align-top me-2"
@@ -28,42 +26,13 @@ const Header = () => {
             />
             Airline FinEco
           </Navbar.Brand>
-
           <Navbar.Toggle aria-controls="navbar-nav" />
           <Navbar.Collapse id="navbar-nav">
             <Nav className="ms-auto">
-
-              {/* Use NavLink for each nav button. 
-                  The 'end' prop ensures exact matching for the home path. */}
-              <Nav.Link
-                as={NavLink}
-                to="/"
-                end
-              >
-                Home
-              </Nav.Link>
-
-              <Nav.Link
-                as={NavLink}
-                to="/data"
-              >
-                Data
-              </Nav.Link>
-
-              <Nav.Link
-                as={NavLink}
-                to="/resources"
-              >
-                Resources
-              </Nav.Link>
-
-              <Nav.Link
-                as={NavLink}
-                to="/about"
-              >
-                About
-              </Nav.Link>
-
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/data">Data</Nav.Link>
+              <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
+              <Nav.Link as={Link} to="/about">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
